16657e61a6207cd818ded9c9368e3b0e5267379b
otsybulsky <helg37@gmail.com>
2018-04-01 17:10:32 +0300

    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Itemli-ext
---
+++ src/popup.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Nested from './nested-component';
+
+class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {tabs: []};    
+  }
+
+  componentDidMount() {
+    this.setState({tabs: []})
+    
+    browser.tabs.query({currentWindow: true}).then(tabs => {
+      tabs.map(tab => {
+        const {index, title, url, favIconUrl} = tab
+        const item = {index, title, url, favIconUrl}
+        this.setState({tabs: [...this.state.tabs, item]})
+      })
+    });
+  }
+
+  renderTabs() {
+    return(
+      this.state.tabs.map(tab => {
+        return(
+          <li><a target="_blank" href={tab.url}>{tab.title}</a></li>
+        )
+      })
+    )
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <h1>React Component</h1>
+        <p>
+          This is an example of a popup UI in React!
+        </p>
+        <ul>
+          {this.renderTabs()}
+        </ul>
+        <Nested />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Popup/>, document.getElementById('app'));
