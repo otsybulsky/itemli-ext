ce69037ec6a4f6c8b48bf1742ff83aeb425751c7
otsybulsky <helg37@gmail.com>
2018-04-01 23:08:47 +0300

    refactor, update deps, and onClick work in my addon

--- src/components/app.js
+++ src/components/app.js
@@ -9,18 +9,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ tabs: [] })
-
-    browser.tabs.query({ currentWindow: true }).then(tabs => {
-      tabs.map(tab => {
-        const { index, title, url, favIconUrl } = tab
-        const item = { index, title, url, favIconUrl }
-        this.setState({ tabs: [...this.state.tabs, item] })
-      })
-    })
+    this.setState({ tabs: [], renderTabs: null })
   }
 
   renderTabs() {
+    if (!this.state.renderTabs) return <div />
     return this.state.tabs.map(tab => {
       return (
         <li className="collection-item">
@@ -32,10 +25,26 @@ class App extends Component {
     })
   }
 
+  onTest() {
+    this.setState({ tabs: [] })
+    browser.tabs.query({ currentWindow: true }).then(tabs => {
+      tabs.map(tab => {
+        const { index, title, url, favIconUrl } = tab
+        const item = { index, title, url, favIconUrl }
+        this.setState({ tabs: [...this.state.tabs, item] })
+      })
+      this.setState({ renderTabs: true })
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <h5>Itemli extension</h5>
+        <button className="btn indigo" onClick={this.onTest.bind(this)}>
+          <i class="material-icons left">input</i>
+          Get tabs list
+        </button>
         <ul className="collection">{this.renderTabs()}</ul>
         <Nested />
       </div>
